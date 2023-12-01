07fe26d5d5f557105736d541ed9378510001a3b1
Sowmya-Raghuram <114737212+Sowmya-Raghuram@users.noreply.github.com>
2023-12-01 16:48:05 +0530

    Volunteering Microservice Setup

---
+++ src/main/java/com/sunbird/serve/volunteering/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sunbird.serve.volunteering;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VolunteeringApplication.class);
+	}
+
+}
